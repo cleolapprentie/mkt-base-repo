34ec350cb6dfe96d7372d9521d14a9f9b68ca6b8
cleo.hsu <kayahino@gmail.com>
2021-04-13 15:54:18 +0800

    讓alias跟著欲執行的專案作用

---
+++ addIntellisence.js
@@ -0,0 +1,16 @@
+const fs = require('fs')
+
+const jsconfig = {
+  compilerOptions: {
+    baseUrl: `./${process.env.PROJECT}`,
+    paths: {
+      '@/*': ['./src/*']
+    }
+  },
+  exclude: ['node_modules', 'dist', '.vscode']
+}
+
+fs.writeFile('./jsconfig.json', JSON.stringify(jsconfig, null, 2), function (err) {
+  if (err) throw err
+  console.log('rewrite alias setting success!')
+})
