4dc0a987ee246cf83cff75b1ea6b17d286acbd36
cleo.hsu <kayahino@gmail.com>
2021-04-16 09:13:37 +0800

    調整pages設定

--- vue.config.js
+++ vue.config.js
@@ -35,18 +35,8 @@ module.exports = {
   },
   pages: {
     index: {
-      // page 的入口
-      entry: `${process.env.PROJECT}/src/main.js`,
-      // 模板来源
-      template: `${process.env.PROJECT}/public/index.html`,
-      // 在 dist/index.html 的输出
-      filename: 'index.html',
-      // 当使用 title 选项时，
-      // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-      title: 'Index Page',
-      // 在这个页面中包含的块，默认情况下会包含
-      // 提取出来的通用 chunk 和 vendor chunk。
-      chunks: ['chunk-vendors', 'chunk-common', 'index']
+      entry: `${projectName}/src/main.js`,
+      template: `${projectName}/public/index.html`
     }
   },
   configureWebpack: (config) => {
@@ -59,7 +49,7 @@ module.exports = {
       },
       plugins: [
         new CopyWebpackPlugin([{
-          from: resolve(`${process.env.PROJECT}/public/`),
+          from: resolve(`${projectName}/public/`),
           to: resolve(buildPath)
         }]),
         new StyleLintPlugin({
